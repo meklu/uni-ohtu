0f52a2cb49e5fc8cde26960749620000141497e6
Melker Narikka <meklu@meklu.org>
2020-11-16 17:34:23 +0200

    v3t1 tehty

--- viikko3/teht1-2/nhlreader/src/main/java/ohtu/Player.java
+++ viikko3/teht1-2/nhlreader/src/main/java/ohtu/Player.java
@@ -1,8 +1,14 @@
+// sample data
+// {"name":"Janne Kuokkanen","nationality":"FIN","assists":0,"goals":0,"penalties":0,"team":"NJD","games":1}
 
 package ohtu;
 
 public class Player {
     private String name;
+    private String nationality;
+    private int assists;
+    private int goals;
+    private String team;
 
     public void setName(String name) {
         this.name = name;
@@ -12,9 +18,41 @@ public class Player {
         return name;
     }
 
+    public String getNationality() {
+        return nationality;
+    }
+
+    public void setNationality(String nationality) {
+        this.nationality = nationality;
+    }
+
+    public int getAssists() {
+        return assists;
+    }
+
+    public void setAssists(int assists) {
+        this.assists = assists;
+    }
+
+    public int getGoals() {
+        return goals;
+    }
+
+    public void setGoals(int goals) {
+        this.goals = goals;
+    }
+
+    public String getTeam() {
+        return team;
+    }
+
+    public void setTeam(String team) {
+        this.team = team;
+    }
+
     @Override
     public String toString() {
-        return name;
+        return name + " team " + team + " goals " + goals + " assists " + assists;
     }
-      
+
 }
