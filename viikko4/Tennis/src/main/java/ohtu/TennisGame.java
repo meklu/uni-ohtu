7e51746c9d44936c4db122bae9c9ac418ff83967
Melker Narikka <meklu@meklu.org>
2020-11-23 20:43:15 +0200

    v4t6 pisteiden nimet keskitetty

--- viikko4/Tennis/src/main/java/ohtu/TennisGame.java
+++ viikko4/Tennis/src/main/java/ohtu/TennisGame.java
@@ -20,29 +20,29 @@ public class TennisGame {
         }
     }
 
+    private String getScoreName(int score) {
+        switch (score) {
+            case 0:
+                return "Love";
+            case 1:
+                return "Fifteen";
+            case 2:
+                return "Thirty";
+            case 3:
+                return "Forty";
+            default:
+                return "";
+        }
+    }
+
     public String getScore() {
         String score = "";
-        int tempScore=0;
-        if (m_score1==m_score2)
-        {
-            switch (m_score1)
-            {
-                case 0:
-                        score = "Love-All";
-                    break;
-                case 1:
-                        score = "Fifteen-All";
-                    break;
-                case 2:
-                        score = "Thirty-All";
-                    break;
-                case 3:
-                        score = "Forty-All";
-                    break;
-                default:
-                        score = "Deuce";
-                    break;
-
+        int tempScore = 0;
+        if (m_score1 == m_score2) {
+            if (m_score1 < 4) {
+                score = getScoreName(m_score1) + "-All";
+            } else {
+                score = "Deuce";
             }
         }
         else if (m_score1>=4 || m_score2>=4)
@@ -55,26 +55,7 @@ public class TennisGame {
         }
         else
         {
-            for (int i=1; i<3; i++)
-            {
-                if (i==1) tempScore = m_score1;
-                else { score+="-"; tempScore = m_score2;}
-                switch(tempScore)
-                {
-                    case 0:
-                        score+="Love";
-                        break;
-                    case 1:
-                        score+="Fifteen";
-                        break;
-                    case 2:
-                        score+="Thirty";
-                        break;
-                    case 3:
-                        score+="Forty";
-                        break;
-                }
-            }
+            score = getScoreName(m_score1) + "-" + getScoreName(m_score2);
         }
         return score;
     }
