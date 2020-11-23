acd2e681fdd2c41eb4d41bc3ce44bb11a1f604df
Melker Narikka <meklu@meklu.org>
2020-11-23 19:08:30 +0200

    v4t4 tehty

--- viikko4/Verkkokauppa/src/test/java/ohtu/verkkokauppa/KauppaTest.java
+++ viikko4/Verkkokauppa/src/test/java/ohtu/verkkokauppa/KauppaTest.java
@@ -215,4 +215,35 @@ public class KauppaTest {
         verify(viite, times(2)).uusi();
         verify(pankki).tilisiirto(eq("tapani"), eq(84), eq("612345"), anyString(), eq(5+7));
     }
+
+    @Test
+    public void tuotteenPoistoKoristaToimii() {
+        // luodaan ensin mock-oliot
+        Pankki pankki = mock(Pankki.class);
+
+        Viitegeneraattori viite = mock(Viitegeneraattori.class);
+        // määritellään että viitegeneraattori palauttaa viitten 42
+        when(viite.uusi()).thenReturn(42);
+
+        Varasto varasto = mock(Varasto.class);
+        // määritellään että tuote numero 1 on maito jonka hinta on 5 ja saldo 10
+        when(varasto.saldo(1)).thenReturn(10);
+        when(varasto.haeTuote(1)).thenReturn(new Tuote(1, "maito", 5));
+        // määritellään että tuote numero 2 on juusto jonka hinta on 7 ja saldo 2
+        when(varasto.saldo(2)).thenReturn(2);
+        when(varasto.haeTuote(2)).thenReturn(new Tuote(2, "juusto", 7));
+
+        // sitten testattava kauppa
+        Kauppa k = new Kauppa(varasto, pankki, viite);
+
+        // tehdään ostokset
+        k.aloitaAsiointi();
+        k.lisaaKoriin(1);     // ostetaan tuotetta numero 1 eli maitoa
+        k.lisaaKoriin(2);     // muistetaan myös juusto leivän päälle
+        k.poistaKorista(1);   // maito olikin virhe
+        k.tilimaksu("pekka", "12345");
+
+        // maitoa ei ostettu
+        verify(pankki).tilisiirto(eq("pekka"), eq(42), eq("12345"), anyString(), eq(7));
+    }
 }
